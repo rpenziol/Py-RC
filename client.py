73e5adee3cc121e49cea54070017d5768bcdae0d
Robbie Penziol <rpenziol@users.noreply.github.com>
2015-11-27 23:13:50 -0800

    Added initial client/server implementation

--- client.py
+++ client.py
@@ -1,16 +1,59 @@
 import socket
+import select
+import sys
+import string
 
-def main():
-    HOST = raw_input("Enter the server IP address you'd like to connect to: ")
-    PORT = 31415
+#Available message buffer size
+RECV_BUFFER = 4096
 
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
+def chat_client():
 
-    user_input = ""
+    if(len(sys.argv) < 3) :
+        print 'Usage : python client.py hostname port'
+        sys.exit()
+
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(2)
+
+    #Attempt connection to remote host
+    try :
+        client_socket.connect((HOST, PORT))
+    except :
+        print 'Unable to connect'
+        sys.exit()
+
+    print('Connected to remote host.')
 
     #Main while loop to send/receive messages
-    while user_input != '/q':
-        user_input = raw_input()
+    while True:
+        CONNECTION_LIST = [sys.stdin, client_socket]
 
-if __name__ == '__main__': main()
+        #Get list of readable sockets
+        read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST,[],[])
+
+        for sock in read_sockets:
+            #Incoming message from server
+            if sock == client_socket:
+                data = sock.recv(RECV_BUFFER)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    sys.stdout.write(data)
+                    prompt()
+
+            #User-entered message
+            else :
+                msg = sys.stdin.readline()
+                client_socket.send(msg)
+                prompt()
+
+def prompt() :
+    sys.stdout.write('<Username> ')
+    sys.stdout.flush()
+
+if __name__ == '__main__':
+    #Exit application if any unhandled exception is thrown
+    sys.exit(chat_client())
--- server.py
+++ server.py
@@ -1,1 +1,63 @@
+import socket
+import select
+import sys
 
+HOST = ''
+PORT = 31415
+#List of connected clients and available message buffer size
+CONNECTION_LIST = []
+RECV_BUFFER = 4096
+
+def chat_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(10)
+
+    #Add server socket to readable connections
+    CONNECTION_LIST.append(server_socket)
+    print "Chat server started on port " + str(PORT)
+
+    while True:
+        #Concurrency control lists
+        read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST,[],[])
+
+        for client in read_sockets:
+            #New connection
+            if client == server_socket:
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                print "Client (%s, %s) connected" % addr
+
+                broadcast_message(server_socket, sockfd, "[%s:%s] is now connected\n" % addr)
+
+            #New message
+            else:
+                try:
+                    data = client.recv(RECV_BUFFER)
+                    if data:
+                        broadcast_message(server_socket, client, "\r" + '<' + str(client.getpeername()) + '> ' + data)
+
+                except:
+                    broadcast_message(server_socket, client, "Client (%s, %s) is offline" % addr)
+                    print "Client (%s, %s) is offline" % addr
+                    client.close()
+                    CONNECTION_LIST.remove(client)
+                    continue
+
+    server_socket.close()
+
+def broadcast_message(server_socket, client_id, message):
+    #Do not send message to server's socket or sending client
+    for client in CONNECTION_LIST:
+        if client != server_socket and client != client_id:
+            try :
+                client.send(message)
+            except :
+                #If unable to send to a socket, close and remove the connection
+                client.close()
+                CONNECTION_LIST.remove(client)
+
+if __name__ == '__main__':
+    #Exit application if any unhandled exception is thrown
+    sys.exit(chat_server())
